6eb902bfe439b9cfe77ba72723980dc47bc3df5c
Cassian <dkkim.esq@Gmail.com>
2025-07-05 19:13:39 +0900

    Add files via upload

--- netlify/functions/verify-password.js
+++ netlify/functions/verify-password.js
@@ -1,1 +1,17 @@
 
+exports.handler = async function(event, context) {
+  const { password } = JSON.parse(event.body || "{}");
+  const VALID_PASSWORD = "trinos2025";
+
+  if (password === VALID_PASSWORD) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true })
+    };
+  } else {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ success: false, message: "Unauthorized" })
+    };
+  }
+};
